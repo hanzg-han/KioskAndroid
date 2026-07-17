eb121c00162c6737320d44cb1e3d6eb621e79c54
韩志刚 <hanzg@wizrole.com>
2026-07-17 15:37:34 +0800

    版本更新至 1.0.36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.kiosk.app"
         minSdk = 30
         targetSdk = 33
-        versionCode = 35
-        versionName = "1.0.35"
+        versionCode = 36
+        versionName = "1.0.36"
     }
 
     buildTypes {
